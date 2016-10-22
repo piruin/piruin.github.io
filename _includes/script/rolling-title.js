88d9174568589991a3ef3f2badb8de27dd2dd1ef
Piruin Panichphol <piruin.p@gmail.com>
2016-10-22 16:50:33 +0700

    refactor

---
+++ _includes/script/rolling-title.js
@@ -0,0 +1,12 @@
+(function(){
+    document.addEventListener('DOMContentLoaded', function(){
+        var children = document.getElementsByClassName('piruin-rolling-title')[0].children;
+        var length = children.length;
+        var idx = 0;
+        setInterval(function(){
+            children[idx].className = "hidden";
+            idx = (idx + 1) % length;
+            children[idx].className = "visible";
+        }, 5000);
+    });
+})();
